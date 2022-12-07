32096b063268adedf7c6428e52a770477532465e
Narcis Badea <badeanarcis@gmail.ocm>
2022-12-07 19:36:43 +0200

    Project init

---
+++ PSSC_E-Comerce/E-Comerce.DAL/Database/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Comerce.DAL.Database;
+
+public class DatabaseContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    public DatabaseContext(DbContextOptions options, IConfiguration configuration) : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("E-ComerceDatabase"));
+    }
+}
